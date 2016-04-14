c521897aaecddcb2f6d8fb982ab049f5d59281ae
Abu Nashir <abunashir@gmail.com>
2016-04-14 11:53:38 +0600

    Business admin can order global certificates
    Business admin can select and order global certificates to sell to
    their customer. Normally global certificate would be setted up by
    the management admin from the owner company.

--- spec/features/admin_orders_certificate_kits_spec.rb
+++ spec/features/admin_orders_certificate_kits_spec.rb
@@ -2,34 +2,50 @@ require "rails_helper"
 
 feature "Order certificate kits" do
   scenario "admin orders certificate kits" do
-    certificate = vist_order_new_certificate_kits
+    certificate = vist_new_certificate_kit_order_page
+    submit_new_certificate_kit_order_form(certificate)
 
-    select certificate.name, from: "order_certificate_id"
-    fill_in "order_quantity", with: 20
-    fill_in "order_note", with: "This is special note"
-    click_on "Create"
+    expect_orders_page_to_have_order_details(certificate)
+  end
 
-    expect(page).to have_content(certificate.name)
-    expect(page).to have_content(20)
-    expect(page).to have_content("Pending")
+  scenario "admin can order global certificate" do
+    certificate = create(:certificate, name: "Global Cert", global: true)
+    vist_new_certificate_kit_order_page
+    submit_new_certificate_kit_order_form(certificate)
+
+    expect_orders_page_to_have_order_details(certificate)
   end
 
   scenario "try to order with invalid information" do
-    vist_order_new_certificate_kits
-
+    vist_new_certificate_kit_order_page
     click_on "Create"
 
     expect(page).to have_content("Certificatecan't be blank")
   end
 
-  def vist_order_new_certificate_kits
+  def vist_new_certificate_kit_order_page
     admin = create(:user, admin: true)
     certificate = create(:certificate, company: admin.company)
 
     visit root_path(as: admin)
     visit marketer_path
+
     click_on "Orders"
     click_on "Add new order"
+
     certificate
+  end
+
+  def submit_new_certificate_kit_order_form(certificate)
+    select certificate.name, from: "order_certificate_id"
+    fill_in "order_quantity", with: 20
+    fill_in "order_note", with: "This is special note"
+    click_on "Create"
+  end
+
+  def expect_orders_page_to_have_order_details(certificate)
+    expect(page).to have_content(certificate.name)
+    expect(page).to have_content(20)
+    expect(page).to have_content("Pending")
   end
 end
